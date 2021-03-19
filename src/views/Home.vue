e7b0d6c4bab4216d570e0dc9ebc83b9ac272c0a0
MuhammadSalman009-tech <salmansumra009@gmail.com>
2021-03-19 17:56:42 +0500

    vue blog initial setup

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <h1>this is home component</h1>
+  {{a}}
+  <button @click="handleClick">Click me</button>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  setup(){
+    const a="salman";
+    const handleClick=()=>{
+      console.log("clicked");
+    }
+    return{a, handleClick}
   }
 }
 </script>
